1c77212cb6d52564b4f6f258046f4dd656f5c354
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-15 10:27:17 -0400

    Add error handlres

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const app = express();
 const authRouter = require("./routes/auth");
 const itemsRouter = require("./routes/items");
 
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Plants nursery");
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
