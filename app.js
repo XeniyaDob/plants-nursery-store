14cc1c6966bb21a8f690ff9cea49ea763e595b6b
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-22 14:46:43 -0400

    Add swagger docs

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 //connect DB
 const connectDB = require("./db/connect");
 
@@ -37,8 +42,9 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-  res.send("Plants store API");
+  res.send('<h1>Plants API</h1><a href="/api-docs">Swagger Docs</a>');
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
