c46ce9da3162087756a5564b2bf6d50b1cc40e93
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-28 10:02:19 -0400

    Create Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PublicLayout from "./components/Layout/PublicLayout";
+import Landing from "./pages/Landing/Landing";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<PublicLayout />}></Route>
+        <Route path="/" element={<PublicLayout />} />
+        <Route index element={<Landing />} />
       </Routes>
     </>
   );
