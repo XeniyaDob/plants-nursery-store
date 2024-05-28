83564ca7e63ab4a0207327dd2318bd13b66a1fce
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-28 12:33:39 -0400

    Create Appbar component

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import PublicLayout from "./components/Layout/PublicLayout";
 import Landing from "./pages/Landing/Landing";
 import AllPlants from "./pages/AllPlants/AllPlants";
-
+import AppBar from "../../client/src/components/AppBar/AppBar";
 function App() {
   return (
     <>
+      <AppBar />
       <Routes>
         <Route path="/" element={<PublicLayout />} />
         <Route index element={<Landing />} />
