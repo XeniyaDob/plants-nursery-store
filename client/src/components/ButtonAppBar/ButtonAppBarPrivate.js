610127bb9fcbf20dc7353a727ab0a4f93bd2af8d
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-29 17:08:01 -0400

    Log out/ remove token info from the app

--- client/src/components/ButtonAppBar/ButtonAppBarPrivate.js
+++ client/src/components/ButtonAppBar/ButtonAppBarPrivate.js
@@ -1,11 +1,20 @@
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 export default function ButtonAppBarPrivate() {
+  let navigate = useNavigate();
   const currentUser = JSON.parse(localStorage.getItem("plantAppUser"));
+
+  const handleLogout = () => {
+    localStorage.removeItem("plantAppToken");
+    localStorage.removeItem("plantAppUser");
+    navigate("/", { replace: true });
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -19,6 +28,9 @@ export default function ButtonAppBarPrivate() {
               {currentUser.name}
             </Typography>
           )}
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
