0e430d174d1ef49b61c7578245fd5c52fd475ae5
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-29 09:03:00 -0400

    Display a name of a user after registration

--- client/src/components/AppBar/AppBar.js
+++ client/src/components/AppBar/AppBar.js
@@ -6,6 +6,8 @@ import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 
 export default function ButtonAppBar() {
+  const currentUser = JSON.parse(localStorage.getItem("plantAppUser"));
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="transparent">
@@ -13,9 +15,15 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Plants Nursery
           </Typography>
-          <Link href="/register" underline="none">
-            Sign up
-          </Link>
+          {currentUser ? (
+            <Typography variant="h6" component="div">
+              {currentUser.name}
+            </Typography>
+          ) : (
+            <Link href="/register" underline="none">
+              Sign up
+            </Link>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
