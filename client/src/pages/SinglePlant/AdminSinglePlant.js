9a009ba8561a7de604ad449a17569f4d61ebdf1b
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-30 13:01:51 -0400

    Get single plant and display it on the page

--- client/src/pages/SinglePlant/AdminSinglePlant.js
+++ client/src/pages/SinglePlant/AdminSinglePlant.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-function AdminSinglePlant() {
-  return <div>AdminSinglePlant</div>;
+export default function AdminSinglePlant() {
+  let params = useParams();
+  const [item, setItem] = useState(null);
+  console.log(item);
+  useEffect(() => {
+    axios
+      .get(`/api/v1/items/${params.id}`)
+      .then((response) => {
+        setItem(response.data.item);
+      })
+      .catch((error) => {
+        console.error("Error fetching item:", error);
+      });
+  }, []);
+
+  return (
+    <Card sx={{ maxWidth: 500, mt: "5rem" }}>
+      <CardMedia sx={{ height: 140 }} title="green iguana" />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {item.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item.description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ mt: "1rem" }}>
+          Price ${item.price}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ mt: "1rem" }}>
+          Created at{" "}
+          {new Date(item.createdAt).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Update</Button>
+        <Button size="small">Delete</Button>
+      </CardActions>
+    </Card>
+  );
 }
-
-export default AdminSinglePlant;
