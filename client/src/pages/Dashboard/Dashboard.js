32b1fb71a50a7afb53a4299e044cda220c31def5
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-30 12:03:54 -0400

    Create a plants table for the admin dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import PlantsTable from "./PlantsTable";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const [allItems, setAllItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/v1/items")
+      .then((response) => {
+        setAllItems(response.data.items);
+      })
+      .catch((error) => {
+        console.error("Error fetching items:", error);
+      });
+  }, []);
+  return (
+    <Box sx={{ mt: "5rem" }}>
+      <PlantsTable allItems={allItems} />
+    </Box>
+  );
 }
