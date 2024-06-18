5da7e012b4955b1f468eeede16d4bbe394053105
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-06-18 13:58:23 -0400

    Refactor Landing page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,19 +1,45 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import backgroundImage from "../../images/pexels-peterfazekas-1089455.jpg";
 
 export default function Landing() {
   return (
-    <Box sx={{ width: "100%" }}>
-      <Typography variant="h2">Plant nursery store Landing page</Typography>
-      <Typography variant="h5" gutterBottom>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur
-      </Typography>
-      <Link href="/items" underline="none">
-        Explore our plants
-      </Link>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#fff",
+        fontSize: "3rem",
+      }}>
+      <Grid container spacing={4} justifyContent="center">
+        <Grid item xs={12} sm={8} md={6}>
+          <Box>
+            <Typography variant="h2" gutterBottom sx={{ fontWeight: "bold" }}>
+              Explore Our Wide Selection of Plants
+            </Typography>
+            <Typography variant="body1" align="center" gutterBottom>
+              From rare orchids to vibrant succulents, we've got everything you
+              need for your garden.
+            </Typography>
+          </Box>
+          <Button
+            variant="outlined"
+            href="/items"
+            sx={{
+              color: "#fff",
+              borderColor: "#fff",
+            }}>
+            Shop Now
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
